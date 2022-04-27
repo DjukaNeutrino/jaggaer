f479fba0fc14fb2e63aa754a710d3b759f6a55fb
djuka <djukacar@gmail.com>
2022-04-27 20:09:50 +0200

    Fetch data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {hot} from "react-hot-loader";
 import CssBaseline from '@mui/material/CssBaseline';
 import "./App.css";
-import data from '../resources/data.json';
+// import data from '../resources/data.json';
 import Header from './components/Header';
 import Grid from '@mui/material/Grid';
 import BasicInfo from './components/BasicInfo';
@@ -10,34 +10,47 @@ import Description from './components/Description';
 import Details from './components/Details';
 import PricingAndShipping from './components/PricingAndShipping';
 import Container from '@mui/material/Container';
+import fetchData from './hooks/fetchData'
 
 const App = ({props}) => {
+    const {
+        data,
+        loading,
+        error
+    } = fetchData();
+
     return <>
-            <CssBaseline />
-            <Header props={props} data={data}/>
-            <Container maxWidth={false}>
-                <Grid container spacing={2} style={{marginTop:'80px'}}>
-                    <Grid item lg={5} md={5} sm={12} xs={12}>
-                        5
+            {loading && <div> Loading... </div>}
+            {error && <div> Error... </div>}
+            {data &&
+            <CssBaseline>
+                <Header props={props} data={data}/>
+                <Container maxWidth={false}>
+                    <Grid container spacing={2} style={{marginTop:'80px'}}>
+                        <Grid item lg={5} md={5} sm={12} xs={12}>
+                            5
+                        </Grid>
+                        <Grid item lg={7} md={7} sm={12} xs={12}>
+                            <BasicInfo data={data}/>
+                        </Grid>
                     </Grid>
-                    <Grid item lg={7} md={7} sm={12} xs={12}>
-                        <BasicInfo data={data}/>
+                    <Grid container spacing={2}>
+                        <Grid item lg={10} md={10} sm={12} xs={12}>
+                            <Description data={data}/>
+                        </Grid>
                     </Grid>
-                </Grid>
-                <Grid container spacing={2}>
-                    <Grid item lg={10} md={10} sm={12} xs={12}>
-                        <Description data={data}/>
+                    <Grid container spacing={2}>
+                        <Grid item lg={5} md={5} sm={5} xs={12}>
+                            <Details data={data}/>
+                        </Grid>
+                        <Grid item lg={5} md={5} sm={5} xs={12}>
+                            <PricingAndShipping data={data}/>
+                        </Grid>
                     </Grid>
-                </Grid>
-                <Grid container spacing={2}>
-                    <Grid item lg={5} md={5} sm={5} xs={12}>
-                        <Details data={data}/>
-                    </Grid>
-                    <Grid item lg={5} md={5} sm={5} xs={12}>
-                        <PricingAndShipping data={data}/>
-                    </Grid>
-                </Grid>
-            </Container>
+                </Container>
+            </CssBaseline>
+            }
+
         </>
 };
 
---
+++ src/hooks/fetchData.js
@@ -0,0 +1,30 @@
+import { useEffect, useState} from 'react';
+import axios from 'axios';
+
+const axiosFetchData = () => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data: response } = await axios.get('/data.json');
+                setData(response);
+            } catch (error) {
+                setError(error)
+            }
+            setLoading(false);
+        };
+
+        return fetchData();
+    }, []);
+
+    return {
+        data,
+        loading,
+        error
+    };
+};
+
+export default axiosFetchData;
